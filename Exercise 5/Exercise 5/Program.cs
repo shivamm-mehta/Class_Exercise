566e0f0bc2ac826799eb3b85d1ed4c58671b7e53
shivamm-mehta <mehtas25@mymacewan.ca>
2024-03-09 19:37:33 -0700

    Exercise uploaded

---
+++ Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine(GetAgeStatus(4));
+        Console.WriteLine(GetAgeStatus(15));
+        Console.WriteLine(GetAgeStatus(24));
+        Console.WriteLine(GetAgeStatus(65));
+        Console.WriteLine(GetAgeStatus(80));
+        Console.WriteLine(GetAgeStatus(-1));
+        Console.WriteLine(GetAgeStatus(200));
+    }
+
+    static string GetAgeStatus(int age)
+    {
+        if (age < 1 || age > 100)
+        {
+            return "Unknown";
+        }
+        else if (age < 6)
+        {
+            return "Toddler";
+        }
+        else if (age < 18)
+        {
+            return "Teen";
+        }
+        else if (age < 65)
+        {
+            return "Adult";
+        }
+        else
+        {
+            return "Senior";
+        }
+    }
+}
