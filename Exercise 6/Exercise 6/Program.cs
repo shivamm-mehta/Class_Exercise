ebaff2fd64343a299c4f5d015b2f2288066f636d
shivamm-mehta <mehtas25@mymacewan.ca>
2024-03-09 19:43:11 -0700

    Exercise uploaded

---
+++ Exercise 6/Exercise 6/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string numbers = "";
+        for (int i = 0; i < 10; i++)
+        {
+            numbers += i;
+        }
+
+        Console.WriteLine($"Numbers: {numbers}");
+
+        Console.WriteLine("Numbers output using foreach: ");
+        foreach (char digit in numbers.ToCharArray())
+        {
+            Console.Write($"{digit}");
+        }
+    }
+}
